4e75801fba6fada6ef88a91df83ab781896d6f87
bkvn <berkerkovan@gmail.com>
2012-06-04 12:53:52 -0400

    Added initial files. Added database connectivity.

---
+++ dinosaurs/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Untitled Document</title>
+    </head>
+
+    <body>
+    	
+        <h2>Velociraptor</h2>
+    	<dl>
+        	<dt>Love Meat</dt>
+            <dd>1</dd>
+            <dt>In Jurassic Park</dt>
+            <dd>1</dd>
+        </dl>
+        
+        <h2>Pterodactyl</h2>
+    	<dl>
+        	<dt>Love Meat</dt>
+            <dd>1</dd>
+            <dt>In Jurassic Park</dt>
+            <dd>0</dd>
+        </dl>
+        
+        <h2>Tyrannosaurus Rex</h2>
+    	<dl>
+        	<dt>Love Meat</dt>
+            <dd>1</dd>
+            <dt>In Jurassic Park</dt>
+            <dd>1</dd>
+        </dl>
+        
+        <h2>Stegosaurus</h2>
+    	<dl>
+        	<dt>Love Meat</dt>
+            <dd>0</dd>
+            <dt>In Jurassic Park</dt>
+            <dd>1</dd>
+        </dl>
+        
+        <h2>Triceratops</h2>
+    	<dl>
+        	<dt>Love Meat</dt>
+            <dd>0</dd>
+            <dt>In Jurassic Park</dt>
+            <dd>1</dd>
+        </dl>
+    
+    </body>
+</html>
